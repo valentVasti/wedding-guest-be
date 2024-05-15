498d1a1ada9a1e46427e9929d232ea44f1112c32
Valent Vasti <valentvasti2114@gmail.com>
2024-05-15 17:25:28 +0700

    feat: add image in public

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Guest List - Wening & Leo Wedding</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
